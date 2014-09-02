1baf4d81a765fe2ec449a0169b670a5087c9f7e1
amurph491 <amurph491@gmail.com>
2014-09-01 21:50:20 -0400

    refactor charting

--- public/javascripts/testchart.js
+++ public/javascripts/testchart.js
@@ -1,25 +1,45 @@
-$(function () { 
-    $('#container').highcharts({
+$(document).ready(function() {
+
+    var options = {
         chart: {
-            type: 'bar'
+            renderTo: 'container_temp',
+            type: 'spline'
         },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
-            }
-        },
-        series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
-        }, {
-            name: 'John',
-            data: [5, 7, 3]
-        }]
+        series: [{}]
+    };
+
+    $.getJSON('/m/serve/53fbf603f2426e0000f3e131', function(result) {
+        console.log(result);
+                
+       var data = result.measurements.map(function(json){
+                        return json.temp;});
+        data = data.map(Number);
+        options.series[0].data = data;
+
+        var chart = new Highcharts.Chart(options);
     });
+
+});
+
+$(document).ready(function() {
+
+    var options = {
+        chart: {
+            renderTo: 'container_sound',
+            type: 'spline'
+        },
+        series: [{}]
+    };
+
+    $.getJSON('/m/serve/53fbf603f2426e0000f3e131', function(result) {
+        console.log(result);
+                
+       var data = result.measurements.map(function(json){
+                        return json.sound;});
+        data = data.map(Number);
+        options.series[0].data = data;
+
+        var chart = new Highcharts.Chart(options);
+    });
+
 });
