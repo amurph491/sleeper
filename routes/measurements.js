5721ad1f5553bb2c88c56c2f2de4ab4dc4102a1a
amurph491 <amurph491@gmail.com>
2014-09-09 22:11:22 -0400

    list measurements route

--- routes/measurements.js
+++ routes/measurements.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/', function(req,res){
+	var db = req.db;
+	var collection = db.get('nightcollection');
+	collection.find({},{},function(e,docs){
+		res.render('nightlist', {
+			"nightlist" : docs
+		});
+	});
+});
+
 router.get('/:nid/up', function(req, res) {
 	var db = req.db;
 	var collection = db.get('nightcollection');
